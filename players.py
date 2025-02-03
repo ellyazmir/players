dc0c80b66f98bc64e5fdfdb039aa8e9ee153d351
ellyazmir <elleyazmir@gmail.com>
2025-02-03 20:05:37 +0800

    players.py

--- players.py
+++ players.py
@@ -1,12 +1,11 @@
 def save_game_txt(PlayerName, game_state, filename = "PlayersData.txt"):
-
-    # Use 'a' mode to append data
+    
+    # 'a' to append data
     with open(filename, 'a') as file:  
         file.write(f"Player: {PlayerName}\n")
         for key, value in game_state.items():
             file.write(f"{key}: {value}\n")
- 
-        # Blank line to separate players' data
+        # Blank line to separate data
         file.write("\n")
 
 
@@ -17,16 +16,18 @@ def load_game_txt(PlayerName, password, filename = "PlayersData.txt"):
         player_found = False
         for line in lines:
 
-            if line.startswith("player:"):
-                current_player = line.split(": ")[1].strip()
-                if current_player == PlayerName:
+            if line.startswith("Player:"):
+
+                # (": ")[1] = player's name
+                CurrentPlayer = line.split(": ")[1].strip()
+                if CurrentPlayer == PlayerName:
+                    # True = data found // False = ignore
                     player_found = True
                 else:
                     player_found = False
 
             elif player_found:
-
-                # End of current player's data
+                # Empty line to indicate the end of a player data
                 if line.strip() == "":
                     break  
                 key, value = line.strip().split(': ')
@@ -34,7 +35,6 @@ def load_game_txt(PlayerName, password, filename = "PlayersData.txt"):
 
     if game_state.get('password') == password:
         return game_state
-    
     else:
         print("Incorrect name or password.")
         return None
@@ -46,45 +46,50 @@ def start_game():
         
         if choice == 'X':
             print("Goodbye!")
-            break
+            return
 
         if choice == 'L':
+            # Limit to 5 attempts only
             attempts = 5
             while attempts > 0:
+
                 PlayerName = input("Enter your name (or 'X' to quit): ")
                 if PlayerName == 'X':
                     print("Goodbye!")
                     return
-
+                
                 password = input("Enter password (or 'X' to quit): ")
                 if password == 'X':
                     print("Goodbye!")
                     return
-
+                
+                # Load game with correct name and password
                 game_state = load_game_txt(PlayerName, password)
                 if game_state:
                     print(f"Welcome back, {PlayerName}!")
                     return
-
+                
+                # Load game with incorrect name or password or both
                 else:
                     attempts -= 1
                     print(f"You have {attempts} attempts left.")
                     if attempts == 0:
                         print("Maximum attempts reached.")
                         print("Goodbye!")
-                        return
+                        break
                     
         elif choice == 'N':
             PlayerName = input("Enter your name (or 'X' to quit): ")
             if PlayerName == 'X':
                 print("Goodbye!")
                 break
-
+            
             password = input("Create a new password (or 'X' to quit): ")
             if password == 'X':
                 print("Goodbye!")
                 break
 
+            # Starter pack
             game_state = {
                 'password': password,
                 'my pet': '0',
@@ -95,6 +100,6 @@ def start_game():
             break
 
         else:
-            print("Invalid. Please enter 'L', 'N', or 'X'.")
+            print("Invalid choice. Please enter 'L', 'N', or 'X'.")
 
 start_game()
